a0af39a0d7533530e05d230aef796b83941c7f46
john maclaine <johnmaclained@email.com>
2022-03-30 10:38:47 +0800

    Initial commit

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useRef} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {  NodePlayerView } from 'react-native-nodemediaclient';
 
 export default function App() {
+  const vp = useRef()
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>test</Text>
+      <NodePlayerView 
+        style={{ height: 500, width: 500 }}
+        ref={vp}
+        inputUrl={"rtmp://192.168.1.244/live/test"}
+        scaleMode={"ScaleAspectFit"}
+        bufferTime={300}
+        maxBufferTime={1000}
+        autoplay={true}
+      />
     </View>
   );
 }
